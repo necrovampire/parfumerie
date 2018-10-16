03f15b09be102dd7d976ab6a417f66fd993d0dba
ezeallio <ezequiel.allio@gmail.com>
2018-10-16 09:42:31 -0300

    Update lote.sql

--- lote.sql
+++ lote.sql
@@ -1,7 +1,7 @@
 USE Perfumerie;
 
 -- Insertamos Productos
-
+TRUNCATE producto;
 INSERT INTO producto (Codigo, Nombre, Costo) VALUES ("ASD", "Perfume 1", 200.50);
 INSERT INTO producto (Codigo, Nombre, Costo) VALUES ("DSA", "Perfume 2", 200.80);
 INSERT INTO producto (Codigo, Nombre, Costo) VALUES ("DAS", "Esencia de Vainilla", 80.00);
@@ -9,7 +9,7 @@ INSERT INTO producto (Codigo, Nombre, Costo) VALUES ("CUL", "Esencia de Frutilla
 INSERT INTO producto (Codigo, Nombre, Costo) VALUES ("MIA", "Perfume 3", 310.00);
 
 -- Insertamos Materias Primas
-
+TRUNCATE materiaprima;
 INSERT INTO materiaprima (Codigo, Descripcion, CostoBase) VALUES ("LAT1", "Frutilla", 50);
 INSERT INTO materiaprima (Codigo, Descripcion, CostoBase) VALUES ("LAT2", "Vainilla", 40);
 INSERT INTO materiaprima (Codigo, Descripcion, CostoBase) VALUES ("LAT4", "Lavanda", 80);
@@ -17,33 +17,35 @@ INSERT INTO materiaprima (Codigo, Descripcion, CostoBase) VALUES ("LAT5", "Flor 
 INSERT INTO materiaprima (Codigo, Descripcion, CostoBase) VALUES ("LAT6", "Almizce", 70);
 
 -- Insertamos elementos en la tabla FormulaUnaria
-
+TRUNCATE formulaunaria;
 INSERT INTO formulaunaria (CodigoProductoFinal, CodigoProductoBase) VALUES ("ASD", "DAS");
 INSERT INTO formulaunaria (CodigoProductoFinal, CodigoProductoBase) VALUES ("DSA", "CUL");
 INSERT INTO formulaunaria (CodigoProductoFinal, CodigoProductoBase) VALUES ("DAS", "CUL");
 INSERT INTO formulaunaria (CodigoProductoFinal, CodigoProductoBase) VALUES ("CUL", "DAS");
 
 -- Insertamos elementos en la tabla FormulaBinaria
-
+TRUNCATE formulabinaria;
 INSERT INTO formulabinaria (CodigoProductoFinal, CodigoMateriaPrima) VALUES ("DSA", "LAT5");
 INSERT INTO formulabinaria (CodigoProductoFinal, CodigoMateriaPrima) VALUES ("MIA", "LAT4");
 INSERT INTO formulabinaria (CodigoProductoFinal, CodigoMateriaPrima) VALUES ("DAS", "LAT2");
 INSERT INTO formulabinaria (CodigoProductoFinal, CodigoMateriaPrima) VALUES ("CUL", "LAT1");
 
 -- Insertamos tipos de IVA
-
+TRUNCATE iva;
 INSERT INTO iva (ID, Descripcion, Valor) VALUES (1, "Repercutido 10", 10);
 INSERT INTO iva (ID, Descripcion, Valor) VALUES (12, "Repercutido 21", 21);
 INSERT INTO iva (ID, Descripcion, Valor) VALUES (40, "Repercutido 4", 4);
 
 -- Insertamos Clientes
 
+TRUNCATE cliente;
 INSERT INTO cliente (CUIT, RazonSocial, Domicilio, Telefono, ID_IVA) VALUES (30710123388, "Personas Jurídicas", "Calle Falsa 123", 44438353, 1);
 INSERT INTO cliente (CUIT, RazonSocial, Domicilio, Telefono, ID_IVA) VALUES (30710123389, "Garcia Armenteros", "Calle Falsa 1234", 44438354, 12);
 INSERT INTO cliente (CUIT, RazonSocial, Domicilio, Telefono, ID_IVA) VALUES (31710123389, "Grupo Modesto", "Calle Falsa 2234", 44438355, 40);
 
 -- Insertamos Pedidos
 
+TRUNCATE pedido;
 INSERT INTO pedido (Descripcion, Fecha) VALUES ("perfumes", utc_date());
 INSERT INTO pedido (Descripcion, Fecha) VALUES ("perfumes2", utc_date());
 INSERT INTO pedido (Descripcion, Fecha) VALUES ("frutilla", utc_date());
